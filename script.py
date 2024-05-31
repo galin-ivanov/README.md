ca68f675dee68a16bd8e218f2d4c6033a1453f73
galin-ivanov <168933004+galin-ivanov@users.noreply.github.com>
2024-05-31 19:36:28 +0300

    Add files via upload

---
+++ script.py
@@ -0,0 +1,15 @@
+print("Това е  лексикона на Марка - искам да знам всичко")
+
+first_name = input("Въведи първо име")
+famyli_name =  input("Въведи фамилия")
+you_are_tall = input("ти си висок")
+and_you_weigh = input("ти тежиш")
+full_name = first_name +" " + famyli_name
+print("Здравей " + full_name)
+print("Ти си висок " + you_are_tall + " метра")
+print("Ти тежиш " + and_you_weigh + " киограма")
+
+
+parfume_millilitres_quality = 150
+parfume_millilitres_signature = "C7544"
+
