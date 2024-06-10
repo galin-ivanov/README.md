5946ad7d22e0896b1447152936e90ed960ab5402
galin-ivanov <168933004+galin-ivanov@users.noreply.github.com>
2024-06-10 18:03:41 +0300

    Add files via upload

--- script.py
+++ script.py
@@ -1,18 +1,46 @@
-print("Това е  лексикона на Марка - искам да знам всичко")
-
-first_name =input ("въведи първо име")
-family_name = input("въведи фамилия")
-you_are_tall = input("ти си висок  метра")
-and_you_weigh = input("и тежиш  киограма")
-
-full_name = first_name + family_name
-print("Здравей " + full_name + " ти си висок " + you_are_tall + " метра" + " и ти тежиш " + and_you_weigh + " киограма ")
-                   
+# задача 5
+price_amount_to_pay = 0
 
 
+print("Това е лексикона на Марка - искам да знам всичко")
+print("===================================")
+
+first_name = input("въведи първо име")
+price_amount_to_pay = price_amount_to_pay + 1.5
+
+print("Ти отговори на въпрос дължиш " + str(price_amount_to_pay ))
+
+
+last_name = input("въведи фамилия")
+price_amount_to_pay = price_amount_to_pay + 1.5
+
+full_name = first_name + " " + last_name
+
+
+user_weight = input(full_name + " колко тежиш")
+price_amount_to_pay = price_amount_to_pay + 1.5
 
 
 
+user_weight_metric = input(full_name + " в каква единица си мериш теглото")\
+price_amount_to_pay = price_amount_to_pay + 1.5
 
 
+user_height = input(full_name + "колко си висок")
+price_amount_to_pay = price_amount_to_pay + 1.5
+
+user_height_metric = input (full_name + " в каква единица си мериш височината?")
+price_amount_to_pay = price_amount_to_pay + 1.5
+
+print(full_name + "ти си висок " + user_height + " " + user_height_metric
+      + " и тежиш " + user_weight + " " + user_weight_metric )
+
+parfume_volume = 150.0
+parfume_signature = "C7544"
+liquore_valume = 5
+liquore_sigrature = "m7441"
+bonbon_count = 25
+bonbon_signature = "S6491"
+money_ammont = 410.54
+money_signature = "P7485"
 
